e6baa6e4d83f92d72894f7c0e77e460ac75eb998
Oleksii Buzilov <slmk445@gmail.com>
2025-11-15 22:24:03 +0200

    Initial commit.

---
+++ src/main/java/com/buzilov/textapocalypse/TextapocalypseApplication.java
@@ -0,0 +1,13 @@
+package com.buzilov.textapocalypse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextapocalypseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextapocalypseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buzilov/textapocalypse/TextapocalypseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzilov.textapocalypse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextapocalypseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
